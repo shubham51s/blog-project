099a2276d20014d1ab13400a4721d795ad885ddb
shubham51s <shubhmasolat51@gmail.com>
2025-07-11 23:36:16 +0530

    signup page UI almost done

--- src/components/Authentication/signup.jsx
+++ src/components/Authentication/signup.jsx
@@ -13,8 +13,50 @@ function SignupComp() {
         <div className="py-12 px-20">
           <h3 className="flex items-center justify-center letter-spacing-4 line-h-5 font-7 color-6 font-medium mb-8">Join Medium.</h3>
           <div>
-            <label className="my-4">Name</label>
-            <input className="w-full h-10 border border-black rounded-md px-4" placeholder="Enter your name" />
+            <div>
+              <div className="margin-6">
+                <label htmlFor="name">Name</label>
+              </div>
+              <input id="name" className="w-full h-10 border border-black border-radius-1 padding-7" type="text" placeholder="Enter full name" />
+            </div>
+            <div>
+              <div className="margin-6">
+                <label htmlFor="email">Email</label>
+              </div>
+              <input id="email" className="w-full h-10 border border-black border-radius-1 padding-7" type="email" placeholder="Enter your email" />
+            </div>
+            <div>
+              <div className="margin-6">
+                <label htmlFor="password">Password</label>
+              </div>
+              <input id="password" className="w-full h-10 border border-black border-radius-1 padding-7" type="password" placeholder="Enter password" />
+            </div>
+            <div>
+              <div className="margin-6">
+                <label htmlFor="confirmPassowrd">Confirm password</label>
+              </div>
+              <input id="confirmPassowrd" className="w-full h-10 border border-black border-radius-1 padding-7" type="password" placeholder="Confirm password" />
+            </div>
+            <div>
+              <div className="margin-6 flex items-center">
+                <div className="margin-3 flex items-center">Gender:</div>
+                <label htmlFor="male" className="mr-3 flex items-center cursor-pointer">
+                  <input className="mr-2" id="male" name="gender" type="radio" value="male" />
+                  Male
+                </label>
+                <label htmlFor="female" className="mr-3 flex items-center cursor-pointer">
+                  <input className="mx-2" id="female" name="gender" type="radio" value="female" />
+                  Female
+                </label>
+                <label htmlFor="other" className="flex items-center cursor-pointer">
+                  <input className="mx-2" id="other" name="gender" type="radio" value="other" />
+                  Other
+                </label>
+              </div>
+              <div className="mt-5 flex items-center w-full justify-center">
+                <button className="padding-6 px-0 w-full bg-blue-800 font-normal color-5 font-9 rounded-full opacity-75 cursor-pointer transition-all ease-in-out duration-200 hover:opacity-100">Submit</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
