79859e6346ead92093369c9f4bff5bdd09012916
shubham51s <shubhmasolat51@gmail.com>
2025-07-11 22:25:05 +0530

    working on signup component

--- src/components/Authentication/signup.jsx
+++ src/components/Authentication/signup.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import CloseIcon from "@mui/icons-material/Close";
 
 function SignupComp() {
+  const [isShowPopup, setIsShowPopup] = useState(true);
+
   return (
     <div className="absolute z-[999] w-full custom-bg-6 h-screen flex items-center justify-center">
-      <div className="relative bg-gray-500 width-5 h-[75%] custom-bg-2 rounded">
+      <div className="relative bg-gray-500 width-5 custom-bg-2 rounded">
         <div className="absolute right-0 top-0 width-4 aspect-square flex items-center justify-center">
-          <CloseIcon className="cursor-pointer w-[50%] opacity-75" />
+          <CloseIcon onclick={() => setIsShowPopup(false)} className="cursor-pointer w-[50%] opacity-75" />
         </div>
-        <div className="p-8 bg-red-400">
-          <h3 className="letter-spacing-4 line-h-5 font-7 color-6 font-normal">Join Medium.</h3>
+        <div className="py-12 px-20">
+          <h3 className="flex items-center justify-center letter-spacing-4 line-h-5 font-7 color-6 font-medium mb-8">Join Medium.</h3>
+          <div>
+            <label className="my-4">Name</label>
+            <input className="w-full h-10 border border-black rounded-md px-4" placeholder="Enter your name" />
+          </div>
         </div>
       </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import UserProvider from "./context/userContext.jsx";
 import "../src/index.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
