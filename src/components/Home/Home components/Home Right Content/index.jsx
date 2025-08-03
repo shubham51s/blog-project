aa508cc8a1d27ac3923bc41425b5dae05cd81041
shubham51s <shubhmasolat51@gmail.com>
2025-08-04 01:38:50 +0530

    home right section UI complete

--- src/components/Home/Home components/Home Right Content/index.jsx
+++ src/components/Home/Home components/Home Right Content/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { IoClose } from "react-icons/io5";
+import { CiBookmarkPlus } from "react-icons/ci";
 
 function HomeRightSectionComp() {
   const staffPickedPosts = [
@@ -34,9 +35,81 @@ function HomeRightSectionComp() {
     },
   ];
 
+  const footerOptions = [
+    { id: 0, name: "Help", path: "/" },
+    { id: 1, name: "Status", path: "/" },
+    { id: 2, name: "About", path: "/" },
+    { id: 3, name: "Careers", path: "/" },
+    { id: 4, name: "Press", path: "/" },
+    { id: 5, name: "Blog", path: "/" },
+    { id: 6, name: "Privacy", path: "/" },
+    { id: 7, name: "Rules", path: "/" },
+    { id: 8, name: "Terms", path: "/" },
+    { id: 9, name: "Text to speech", path: "/" },
+  ];
+
   const index = 3;
 
   const [isShowWritingSection, setIsShowWritingSection] = useState(true);
+  const [RecommendedTopics, setRecommendedTopics] = useState([
+    {
+      id: 0,
+      name: "Programming",
+      path: "/",
+    },
+    {
+      id: 1,
+      name: "Writing",
+      path: "/",
+    },
+    {
+      id: 3,
+      name: "Self Improvement",
+      path: "/",
+    },
+    {
+      id: 4,
+      name: "Data Science",
+      path: "/",
+    },
+    {
+      id: 5,
+      name: "Technology",
+      path: "/",
+    },
+    {
+      id: 6,
+      name: "Relationships",
+      path: "/",
+    },
+    {
+      id: 7,
+      name: "Cryptocurrency",
+      path: "/",
+    },
+  ]);
+
+  const [peopleToFollow, showPeopleToFollow] = useState([
+    {
+      id: 0,
+      name: "Xinran Ma",
+      profileImg: "https://miro.medium.com/v2/resize:fill:64:64/1*vApbrRD-tGko7Ifvhzc3cg.png",
+      path: "/",
+    },
+    {
+      id: 1,
+      name: "Personal Growth",
+      profileImg: "https://miro.medium.com/v2/resize:fill:64:64/1*vT9qT49CpcgqXBZAh-X3cA.jpeg",
+      path: "/",
+    },
+    {
+      id: 2,
+      name: "Mark Simmons",
+      profileImg: "https://miro.medium.com/v2/resize:fill:64:64/1*uPc0nFQIyN2ZCAuitoZZ2g.jpeg",
+      path: "/",
+    },
+  ]);
+
   return (
     <div className="width-22 width-21 height-13 bdr-5 padding-3 custom-bg-8 padding-24" style={{ borderRight: 0, borderBlock: 0, paddingBlock: 0 }}>
       <div className="relative inline-block h-full w-full">
@@ -132,16 +205,105 @@ function HomeRightSectionComp() {
                     </div>
                   </div>
                 </div>
-                {/* section 2 working */}
-                <div className="margin-22" style={{ marginBottom: 0, marginInline: 0 }}></div>
-                {/* section 3 */}
-                <div className="margin-22" style={{ marginBottom: 0, marginInline: 0 }}></div>
-                {/* section 4 */}
-                <div className="margin-22" style={{ marginBottom: 0, marginInline: 0 }}></div>
+
+                <div className="margin-22" style={{ marginBottom: 0, marginInline: 0 }}>
+                  <div>
+                    <div className="padding-3" style={{ paddingTop: 0, paddingInline: 0 }}>
+                      <h2 className="tracking-normal font-10 font-medium color-3 custom-line-h-1 m-0 p-0">Recommended topics</h2>
+                    </div>
+                    <div className="flex items-start flex-wrap">
+                      {RecommendedTopics.map((item) => (
+                        <div className="margin-10 flex" style={{ marginTop: 0 }} key={item.id}>
+                          <Link to={item.path} className="margin-9 cursor-pointer p-0 m-0 no-underline" style={{ marginLeft: 0, marginBlock: 0 }}>
+                            <div className="custom-fs-1 color-3 bg-11 border-radius-6 whitespace-nowrap padding-5 font-medium">{item.name}</div>
+                          </Link>
+                        </div>
+                      ))}
+                    </div>
+                    <div className="margin-6" style={{ marginBottom: 0 }}>
+                      <p className="custom-fs-1 color-4 custom-line-h-1 font-medium m-0 p-0">
+                        <Link to="/" className="cursor-pointer m-0 p-0 no-underline">
+                          See more topics
+                        </Link>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="margin-22" style={{ marginBottom: 0, marginInline: 0 }}>
+                  <div>
+                    <div className="padding-18" style={{ paddingBottom: 0 }}>
+                      <div>
+                        <div className="padding-3" style={{ paddingTop: 0, paddingInline: 0 }}>
+                          <h2 className="font-10 font-medium color-3 custom-line-h-1 m-0 p-0">Who to follow</h2>
+                        </div>
+                      </div>
+                      <div>
+                        {peopleToFollow.slice(0, 3).map((item) => (
+                          <div className="h-full w-full" key={item.id}>
+                            <div className="padding-18 flex items-start justify-between w-full" style={{ paddingTop: 0 }}>
+                              <div className="flex items-center justify-center">
+                                <a href="#" className="no-underline shrink-0">
+                                  <div className="relative">
+                                    <img src={item.profileImg} alt={item.name} className="width-11 aspect-square align-middle" style={{ borderRadius: "50%" }} />
+                                  </div>
+                                </a>
+                                <div className="margin-16 overflow-hidden text-ellipsis" style={{ marginBlock: 0 }}>
+                                  <a href="#" className="cursor-pointer m-0 p-0 no-underline overflow-hidden text-ellipsis">
+                                    <h2 className="height-15 font-bold overflow-hidden text-ellipsis font-10 color-3 custom-line-h-1 m-0 p-0">{item.name}</h2>
+                                  </a>
+                                </div>
+                              </div>
+                              <div className="width-23">
+                                <div className="inline-block">
+                                  <button className="flex items-center justify-center bdr-7 padding-20 padding-28 width-24 border-radius-7 bg-transparent cursor-pointer m-0">
+                                    <span className="color-3 custom-fs-1 custom-line-h-1 w-full font-normal">
+                                      <span className="break-all inline-block">Follow</span>
+                                    </span>
+                                  </button>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                    <div className="padding-18" style={{ paddingBottom: 0 }}>
+                      <p className="custom-fs-1 color-4 custom-line-h-1 m-0 p-0 font-normal">
+                        <Link to="/" className="cursor-pointer m-0 p-0 no-underline">
+                          See more suggestions
+                        </Link>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="margin-22" style={{ marginBottom: 0, marginInline: 0 }}>
+                  <div>
+                    <h2 className="font-10 font-medium color-3 custom-line-h-1 m-0 p-0">Reading list</h2>
+                    <div className="padding-18" style={{ paddingBottom: 0 }}>
+                      <p className="custom-fs-1 color-4 custom-line-h-1 m-0 p-0 font-normal">
+                        Click the{" "}
+                        <div className="inline-block custom-h-2 aspect-square margin-23 align-middle">
+                          <CiBookmarkPlus className="w-full h-full" style={{ marginTop: 0, marginInline: 0 }} />
+                        </div>{" "}
+                        on any story to easily add it to your reading list or a custom list that you can share.
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
             {/* footer */}
-            <div className="flex padding-3 flex-wrap" style={{ paddingInline: 0 }}></div>
+            <div className="flex padding-3 flex-wrap" style={{ paddingInline: 0 }}>
+              {footerOptions.map((item) => (
+                <div className="margin-24" style={{ marginLeft: 0, marginBlock: 0 }} key={item.id}>
+                  <a href="#" className="no-underline cursor-pointer m-0 p-0">
+                    <p className="font-8 line-h-7 color-4 font-normal m-0 p-0">{item.name}</p>
+                  </a>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
