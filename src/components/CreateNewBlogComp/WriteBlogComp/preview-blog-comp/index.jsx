df8f6e522770e7d41b5bcb042dc5234aaeac2c97
shubham51s <shubhmasolat51@gmail.com>
2025-10-09 01:45:03 +0530

    working on /create-new page confirm changes UI

--- src/components/CreateNewBlogComp/WriteBlogComp/preview-blog-comp/index.jsx
+++ src/components/CreateNewBlogComp/WriteBlogComp/preview-blog-comp/index.jsx
@@ -12,8 +12,27 @@ function PreviewBlogComp() {
                 <IoCloseOutline className="w-full h-full" />
               </div>
             </button>
-            {/* working */}
-            <div className="custom-line-h-1 font-10 text-left w-[50%] padding55 grow shrink basis-auto"></div>
+
+            <div className="custom-line-h-1 font-10 text-left w-[50%] padding55 grow shrink basis-auto">
+              <p className="font14 font-bold line-h-8 color11 margin-10 p-0 transition-all duration-200 ease-in-out opacity-75 hover:opacity-100" style={{ marginTop: 0, marginInline: 0 }}>
+                Story Preview
+              </p>
+
+              <div className="bg15 w-full">
+                <div className="height67 relative">
+                  <button className="inline-block height68 absolute positionCenter line-h11 padding-38 bdr12 border-radius-9 whitespace-nowrap font-10 text-center align-bottom cursor-pointer select-none box-border font-normal bg16 color-2">Change preview image</button>
+                  <div>
+                    <img src="https://cdn-images-1.medium.com/fit/c/880/400/1*En4Eq_T7gcTS9F-9aZXqwQ.png" alt="" className="w-full height67" />
+                  </div>
+                </div>
+                {/* working */}
+                <div className="bg15 relative"></div>
+              </div>
+
+              <div className="w-full margin46" style={{ marginBottom: 0, marginInline: 0 }}></div>
+
+              <p className="font-10 font-normal custom-line-h-1 color10 margin-11 margin47 p-0" style={{ marginInline: 0 }}></p>
+            </div>
 
             <div className="custom-line-h-1 font-10 text-left w-[50%] padding55 grow shrink basis-auto"></div>
           </div>
