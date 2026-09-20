44a8da5fb2f09de5979b2af05c3196a74059c026
shubham51s <shubhamsolat51@gmail.com>
2026-09-20 10:44:41 +0530

    get notifications time updated

--- src/components/Common/Header/Notification/index.jsx
+++ src/components/Common/Header/Notification/index.jsx
@@ -22,7 +22,7 @@ function Notification() {
             getUnreadNotificationCount();
             timeoutRef.current = null;
           },
-          1000 * 60 * 2,
+          1000 * 60 * 1,
         );
       }
     } catch (err) {
